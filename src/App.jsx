ad218a8398aea68a08a4f60c3e1d96a835815ab6
natasha sibanda <nsibanda374@gmail.com>
2025-10-15 03:56:07 +0200

    First initial off the app

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-green-100 flex items-center justify-center">
+      <h1 className="text-3xl font-bold text-green-700">Hello Tailwind!</h1>
+    </div>
+  );
+}
+
+export default App;
+
